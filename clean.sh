2773af321f18fbb884ce373e27bdcf591940ae6d
Jucalee <anderson.santana7@fatec.sp.gov.br>
2019-10-30 23:30:00 +0000

    DEPLOY

---
+++ clean.sh
@@ -0,0 +1,5 @@
+ssh root@137.74.194.139 <<EOF
+cd AulaHaskell &&
+stack clean &&
+echo "limpeza de arquivos ok!"
+EOF
--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -10,4 +10,27 @@ import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+    defaultLayout $
+        -- remoto
+        -- addScriptRemote "link min do JQuery"
+        -- está no projeto
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead [julius|
+            function ola() {
+                alert("OLA MUNDO");
+            }
+        |]
+        toWidgetHead [lucius|
+            h1 {
+                color : red;
+            }
+        |]
+        [whamlet|
+            <div>
+                <h1> 
+                    OLA MUNDO
+                    
+            <button class"btn btn-danger" onclick="ola()">
+                OK
+        |]
