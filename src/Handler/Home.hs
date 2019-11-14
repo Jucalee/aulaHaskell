358a982ebaad538ead7ce78742676b1b8cd2044e
Jucalee <anderson.santana7@fatec.sp.gov.br>
2019-11-14 00:41:35 +0000

    Ajustes 3

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -51,30 +51,29 @@ getHomeR = do
         |]
         [whamlet|
             <div>
-                <h1> 
+                <h1>
                     OLA MUNDO
-                    
+
             <ul>
                 <li>
-                    <a href=@{Page1R}
+                    <a href=@{Page1R}>
                         Pagina 1
                         
                 <li>
-                    <a href=@{Page2R}
+                    <a href=@{Page2R}>
                         Pagina 2
-                        
+
                 $maybe nome <- sess
                     <li>
                         <div>
                             Ola #{nome}
                         <form method=post action=@{SairR}>
-                            <input type="submit" value="Sair"
+                            <input type="submit" value="Sair">
                             
                 $nothing
                     <li>
                         <div>
                             CONVIDADO
-                        
                     
             <button class"btn btn-danger" onclick="ola()">
                 OK
