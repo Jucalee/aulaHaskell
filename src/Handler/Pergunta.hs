5e84d66c6692cd3c6ab903c42da2a6748799ea42
Jucalee <anderson.santana7@fatec.sp.gov.br>
2019-12-02 18:39:14 +0000

    Alteracao na query

--- src/Handler/Pergunta.hs
+++ src/Handler/Pergunta.hs
@@ -54,7 +54,7 @@ getListQuestR :: Handler Html
 getListQuestR = do
     let sql = "SELECT ??, ?? FROM pergunta \
           \ INNER JOIN alternativa ON pergunta.id = alternativa.perguntaid"
-    perguntas <- runDB $ rawSql sql [] :: Handler [(Entity Pergunta,Entity Alternativa)]
+    perguntas <- runDB $ rawSql sql [] --:: Handler [(Entity Pergunta,Entity Alternativa)]
     defaultLayout $ do
         addStylesheet (StaticR css_bootstrap_css)
         $(whamletFile "templates/list-questions.hamlet")
