8e3b4fbd2e0f4bd0225abc716dc3474746769d46
Jucalee <anderson.santana7@fatec.sp.gov.br>
2019-12-03 21:26:44 +0000

    Adicionando page banda

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -12,6 +12,19 @@ import Text.Lucius
 import Text.Julius
 import Database.Persist.Postgresql
   
+  
+getPage2R :: Handler Html 
+getPage2R = do 
+    defaultLayout $ do 
+        $(whamletFile "templates/page2.hamlet")
+
+getPage1R :: Handler Html 
+getPage1R = do 
+    defaultLayout $ do 
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(juliusFile "templates/page1.julius")
+        toWidgetHead $(luciusFile "templates/page1.lucius")
+        $(whamletFile "templates/page1.hamlet")
 
 getHomeR :: Handler Html
 getHomeR = do
