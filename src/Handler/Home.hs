062ce51221f324197f37218c39a15572dde2b833
Jucalee <anderson.santana7@fatec.sp.gov.br>
2019-12-04 01:06:58 +0000

    Adicionando pagina de sobre

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -78,8 +78,19 @@ getQuizR = do
 getSobreR :: Handler Html
 getSobreR = do
     defaultLayout $ do
+        addStylesheet (StaticR css_animate_css)
+        addStylesheet (StaticR css_icomoon_css)
         addStylesheet (StaticR css_bootstrap_css)
-        [whamlet|
-            <h1>
-                Sobre
-        |]
+        addStylesheet (StaticR css_superfish_css)
+        addStylesheet (StaticR css_style_css)
+        addScript (StaticR js_jquery_js)
+        addScript (StaticR js_modernizr_js)
+        addScript (StaticR js_easing_js)
+        addScript (StaticR js_bootstrap_js)
+        addScript (StaticR js_waypoints_js)
+        addScript (StaticR js_stellar_js)
+        addStylesheetRemote "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300"
+        toWidget $(juliusFile "templates/julius/hoverIntent.julius")
+        toWidget $(juliusFile "templates/julius/superfish.julius")
+        toWidget $(juliusFile "templates/julius/main.julius")
+        $(whamletFile "templates/sobre.hamlet")
