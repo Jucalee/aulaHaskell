3a2bb4312f4f6c952367ef05a4de31b4b21c82b1
Jucalee <anderson.santana7@fatec.sp.gov.br>
2019-12-04 02:43:15 +0000

    Criação de template do quiz.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -70,22 +70,37 @@ getQuizPerguntaR :: Handler Value
 getQuizPerguntaR = do
     perguntas <- runDB $ selectList [] [Asc PerguntaId]
     returnJson (map entityVal perguntas)
+    
+getQuizRespostaR :: Handler Value
+getQuizRespostaR = do
+    alternativas <- runDB $ selectList [] [Asc AlternativaId]
+    returnJson (map entityVal alternativas)
         
 getQuizR :: Handler Html
 getQuizR = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_animate_css)
+        addStylesheet (StaticR css_icomoon_css)
+        addStylesheet (StaticR css_bootstrap_css)
+        addStylesheet (StaticR css_superfish_css)
+        addStylesheet (StaticR css_style_css)
+        addScript (StaticR js_jquery_js)
+        addScript (StaticR js_modernizr_js)
+        addScript (StaticR js_easing_js)
+        addScript (StaticR js_bootstrap_js)
+        addScript (StaticR js_waypoints_js)
+        addScript (StaticR js_stellar_js)
+        addStylesheetRemote "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300"
+        toWidget $(juliusFile "templates/julius/hoverIntent.julius")
+        toWidget $(juliusFile "templates/julius/superfish.julius")
+        toWidget $(juliusFile "templates/julius/main.julius")
+        toWidget $(juliusFile "templates/julius/quiz.julius")
+        $(whamletFile "templates/quiz.hamlet")
     
 --     getImagesR = do
 --   images <- runDB $ selectList [ImagesFilename !=. ""] [Desc ImagesDate]
 --   returnJson (map entityVal images)
     
-    defaultLayout $ do
-        addStylesheet (StaticR css_bootstrap_css)
-        --toWidget $(juliusFile "templates/julius/quiz.julius")
-        [whamlet|
-            <h1>
-                QUIZ
-        |]
-        
 getSobreR :: Handler Html
 getSobreR = do
     defaultLayout $ do
